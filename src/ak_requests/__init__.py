b5446789cf846d5ae324386e10837bb91ba6cb97
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2024-04-30 14:17:57 -0700

    Update __init__.py

--- src/ak_requests/__init__.py
+++ src/ak_requests/__init__.py
@@ -1,5 +1,5 @@
 "Requests package with QOL improvements and anti-bot detection measures"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from ak_requests.request import RequestsSession
 from ak_requests.beautifulsoup import soupify
