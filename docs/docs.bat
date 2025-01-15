793fc21734c7d091a37dea5ad4e1b070283ad06a
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2025-01-15 07:41:27 -0800

    automated doc build

--- docs/docs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Change the current working directory to the directory of the batch script
-cd /d %~dp0
-cd ..
-
-uv run --dev pdoc --docformat google --math --mermaid ak_requests -o docs
